2bdd8a9dfbab3a3372fe8ce725ce2293cce651c1
Dan Lafey <djlafey@gmail.com>
2017-01-01 13:23:18 -0500

    Missed insutrument changes in last commit

--- shared-mc/src/main/java/com/splunk/sharedmc/utilities/Instrument.java
+++ shared-mc/src/main/java/com/splunk/sharedmc/utilities/Instrument.java
@@ -6,21 +6,19 @@ import java.util.List;
 public class Instrument {
 
     private String item;
+    private String name;
     private List enchantments;
 
-
     public Instrument(String item) {
-
         this.enchantments = new ArrayList();
         this.item = item;
     }
 
-
     public void addEnchantment(String item) {
-
-
-        enchantments.add(item);
+        this.enchantments.add(item);
     }
 
-
+    public void setName(String name){
+        this.name = name;
+    }
 }
